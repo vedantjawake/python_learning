c307dfbdb1571a932dbd19135d5d722588009edb
Vedant Jawake <vedantjawake6@gmail.com>
2026-07-29 23:37:45 +0530

    Add Python tuple

--- tuple.py
+++ tuple.py
@@ -3,3 +3,6 @@ mytuple = ("apple", "banana", "cherry")
 thistuple = ("apple", "banana", "cherry")
 print(thistuple) #constructor
 
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
