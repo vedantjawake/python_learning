c4ca7c926e696fa4bb5c20c6e5dc1a3b24ea335f
Vedant Jawake <vedantjawake6@gmail.com>
2026-07-29 23:32:46 +0530

    Add Python lists operation

---
+++ lists.py
@@ -0,0 +1,8 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist)) #constructor
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
