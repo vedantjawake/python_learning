b014db8769265ce25ebbb5498996198c7922dce3
Vedant Jawake <vedantjawake6@gmail.com>
2026-07-30 23:53:30 +0530

    Add Python list operations

--- lists.py
+++ lists.py
@@ -1,74 +1,113 @@
-thislist = ["apple", "banana", "cherry"]
-print(thislist)
+# ==========================================
+# Python List - All Operations
+# ==========================================
 
-mylist =["vedant", "omm", "mayur"]
-print(mylist)
+# 1. Create List
+mylist = ["vedant", "omm", "mayur"]
+print("1. Original List:", mylist)
 
-print(len(mylist)) #length of list
+# 2. Length of List
+print("2. Length:", len(mylist))
 
-print(mylist[0])
+# 3. Access Elements
+print("3. First Element:", mylist[0])
+print("4. Last Element:", mylist[-1])
 
-print(mylist[-1])
-
+# 4. Change Element
 mylist[1] = "rahul"
-print(mylist)
+print("5. After Updating:", mylist)
 
+# 5. Append
 mylist.append("akash")
-print(mylist)
+print("6. After Append:", mylist)
 
+# 6. Insert
 mylist.insert(1, "rohit")
-print(mylist)
+print("7. After Insert:", mylist)
 
+# 7. Extend
 mylist.extend(["ajay", "karan"])
-print(mylist)
+print("8. After Extend:", mylist)
 
-mylist.remove("omm")
-print(mylist)
+# 8. Remove
+mylist.remove("rahul")
+print("9. After Remove:", mylist)
 
+# 9. Pop Last Element
 mylist.pop()
-print(mylist)
+print("10. After pop():", mylist)
 
+# 10. Pop by Index
 mylist.pop(1)
-print(mylist)
+print("11. After pop(1):", mylist)
 
-del mylist[1]
-print(mylist)
+# 11. Membership Operators
+print("12. Is 'vedant' Present?", "vedant" in mylist)
+print("13. Is 'sachin' Not Present?", "sachin" not in mylist)
 
-mylist.clear()
-print(mylist)
+# 12. Index
+print("14. Index of 'mayur':", mylist.index("mayur"))
 
-print("vedant" in mylist)
+# 13. Count
+mylist.append("vedant")
+print("15. Count of 'vedant':", mylist.count("vedant"))
 
-print("sachin" not in mylist)
-
-print(mylist.index("omm"))
-
+# 14. Reverse
 mylist.reverse()
-print(mylist)
+print("16. Reversed List:", mylist)
 
+# 15. Sort
+mylist.sort()
+print("17. Sorted List:", mylist)
+
+# 16. Copy
 newlist = mylist.copy()
+print("18. Copied List:", newlist)
 
-print(newlist)
-
+# 17. Concatenate Lists
 friends = ["rohit", "akash"]
+combined = mylist + friends
+print("19. Combined List:", combined)
 
-newlist = mylist + friends
+# 18. Repeat List
+print("20. Repeated List:", mylist * 2)
 
-print(newlist)
+# 19. Slicing
+print("21. First Two Elements:", mylist[0:2])
+print("22. From Index 1:", mylist[1:])
+print("23. Reverse using Slicing:", mylist[::-1])
 
-print(mylist * 2)
-
-print(mylist[0:2])
-
-print(mylist[1:])
-
-print(mylist[::-1])
-
+# 20. Loop
+print("24. Loop through List:")
 for name in mylist:
     print(name)
 
-thislist = ["apple", "banana", "cherry"]
-print(len(thislist)) #constructor
+# 21. Enumerate
+print("25. Enumerate:")
+for index, name in enumerate(mylist):
+    print(index, name)
 
-mylist = ["apple", "banana", "cherry"]
-print(type(mylist))
+# 22. List Comprehension
+upper_names = [name.upper() for name in mylist]
+print("26. Uppercase List:", upper_names)
+
+# 23. Max and Min
+print("27. Maximum:", max(mylist))
+print("28. Minimum:", min(mylist))
+
+# 24. Delete Element
+del mylist[1]
+print("29. After del:", mylist)
+
+# 25. Clear List
+mylist.clear()
+print("30. After clear():", mylist)
+
+# 26. Type
+thislist = ["apple", "banana", "cherry"]
+print("31. Type:", type(thislist))
+
+# 27. Constructor
+thislist = list(("apple", "banana", "cherry"))
+print("32. Constructor List:", thislist)
+print("33. Length of Constructor List:", len(thislist))
