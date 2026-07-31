b08789a1ce8d70459ffea81eaf0f27ec8d1c89de
Vedant Jawake <vedantjawake6@gmail.com>
2026-07-31 23:56:04 +0530

    Python basics - Functions and Variables

--- Day_01_Functions_and_Variables/variables.py
+++ Day_01_Functions_and_Variables/variables.py
@@ -1,1 +1,11 @@
 # the program file shows   an variable into python 
+
+name = "Vedant"
+age = 23
+height = 5.8
+is_student = True
+
+print("name")
+print("age")
+print("height")
+print("is_student")
