ae66e5a98b6d4306280ae1db7ed6f27c1e83af71
Vedant Jawake <vedantjawake6@gmail.com>
2026-07-31 23:59:18 +0530

    Python basics - Functions and Variables

--- Day_01_Functions_and_Variables/variables.py
+++ Day_01_Functions_and_Variables/variables.py
@@ -1,11 +1,11 @@
 # the program file shows   an variable into python 
-
+vairable_name =ValueError
 name = "Vedant"
 age = 23
 height = 5.8
 is_student = True
 
-print("name")
-print("age")
-print("height")
-print("is_student")
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
